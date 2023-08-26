3d317485b3d1ee20de37e2cc47d2d4c09d71fa23
Thorsten Hindermann <hindermath@googlemail.com>
2023-08-27 00:02:15 +0200

    Die ersten Teile für die Ausgabe in eine Tabelle einer Tabellenkalkulation implementiert. Auch erte Teile für die PDF-Ausgabe.

--- DXDPAPICmdlets/DevExpress.DP.Excel.Cmdlets.cs
+++ DXDPAPICmdlets/DevExpress.DP.Excel.Cmdlets.cs
@@ -1,14 +1,21 @@
-﻿using System.Management.Automation;
+﻿using System.Collections;
+using System.Diagnostics;
+using DXDPAPICmdlets.DataHelper;
+using System.Management.Automation;
 using System.Management.Automation.Internal;
+using DevExpress.Spreadsheet;
 
 namespace DXDPAPICmdlets
 {
     [Cmdlet(VerbsData.Save, "DxDpExcelFile")]
     [Alias("SavAsExcelFile")]
-    public class SaveDxDpExcelCmdlet : PSCmdlet
+    public class SaveDxDpExcelCmdlet : PSCmdlet, IDisposable
     {
         #region Properties
         private List<PSObject> _psObjects = new List<PSObject>();
+        private ProcessObject _processObject = new ProcessObject();
+        private ErrorRecord? _errorRecord;
+        private DXDPAPICmdlets.Models.DataTable _dataTable;
         #endregion Properties
         #region Input Parameters
         /// <summary>
@@ -17,6 +24,10 @@ namespace DXDPAPICmdlets
         /// </summary>
         [Parameter(ValueFromPipeline = true, HelpMessage = "Specifies the input pipeline object")]
         public PSObject InputObject { get; set; } = AutomationNull.Value;
+        [Parameter(HelpMessage = "Open th file in the associated word processor")]
+        public SwitchParameter OpenFileInSpreadsheet { get; set; }
+        [Parameter(Mandatory = true, HelpMessage = "Specifies the file name")]
+        public string FileName { get; set; }
         #endregion
         protected override void BeginProcessing()
         {
@@ -34,6 +45,15 @@ namespace DXDPAPICmdlets
         {
             if (InputObject.Equals(null) || InputObject.Equals(AutomationNull.Value))
                 return;
+
+            if (InputObject.BaseObject is IDictionary dictionary)
+            {
+                foreach (DictionaryEntry entry in dictionary)
+                    if (_processObject.TryProcessObject(PSObject.AsPSObject(entry), ref _psObjects, out _errorRecord))
+                        ThrowTerminatingError(_errorRecord);
+            }
+            else if (_processObject.TryProcessObject(InputObject, ref _psObjects, out _errorRecord))
+                ThrowTerminatingError(_errorRecord);
         }
         protected override void EndProcessing()
         {
@@ -42,6 +62,35 @@ namespace DXDPAPICmdlets
             // Rückkehr, wenn keine Objekte vorhanden sind
             if (_psObjects.Count.Equals(0))
                 return;
+
+            TypeGetter typeGetter = new TypeGetter(this);
+
+            _dataTable = typeGetter.CastObjectsToTableView(_psObjects);
+
+            using (Workbook workbook = new Workbook())
+            {
+                Worksheet worksheet = workbook.Worksheets[0];
+                workbook.BeginUpdate();
+                try
+                {
+                    worksheet.Import(_dataTable.Data, 0, 0);
+                }
+                finally
+                {
+                    workbook.EndUpdate();
+                }
+                workbook.Calculate();
+                workbook.SaveDocument(FileName, DocumentFormat.Xlsx);
+            }
+
+
+            if (OpenFileInSpreadsheet)
+                Process.Start(new ProcessStartInfo(FileName) {UseShellExecute = true});
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- DXDPAPICmdlets/DexExpress.DP.PDF.Cmdlets.cs
+++ DXDPAPICmdlets/DexExpress.DP.PDF.Cmdlets.cs
@@ -1,5 +1,8 @@
-﻿using System.Management.Automation;
+﻿using System.Collections;
+using DXDPAPICmdlets.DataHelper;
+using System.Management.Automation;
 using System.Management.Automation.Internal;
+using DevExpress.Pdf;
 
 namespace DXDPAPICmdlets
 {
@@ -9,6 +12,9 @@ namespace DXDPAPICmdlets
     {
         #region Properties
         private List<PSObject> _psObjects = new List<PSObject>();
+        private ProcessObject _processObject = new ProcessObject();
+        private ErrorRecord? _errorRecord;
+        private DXDPAPICmdlets.Models.DataTable _dataTable;
         #endregion Properties
         #region Input Parameters
         /// <summary>
@@ -17,6 +23,10 @@ namespace DXDPAPICmdlets
         /// </summary>
         [Parameter(ValueFromPipeline = true, HelpMessage = "Specifies the input pipeline object")]
         public PSObject InputObject { get; set; } = AutomationNull.Value;
+        [Parameter(HelpMessage = "Open th file in the associated word processor")]
+        public SwitchParameter OpenFileInPdfApp { get; set; }
+        [Parameter(Mandatory = true, HelpMessage = "Specifies the file name")]
+        public string FileName { get; set; }
         #endregion
         protected override void BeginProcessing()
         {
@@ -35,6 +45,15 @@ namespace DXDPAPICmdlets
         {
             if (InputObject.Equals(null) || InputObject.Equals(AutomationNull.Value))
                 return;
+
+            if (InputObject.BaseObject is IDictionary dictionary)
+            {
+                foreach (DictionaryEntry entry in dictionary)
+                    if (_processObject.TryProcessObject(PSObject.AsPSObject(entry), ref _psObjects, out _errorRecord))
+                        ThrowTerminatingError(_errorRecord);
+            }
+            else if (_processObject.TryProcessObject(InputObject, ref _psObjects, out _errorRecord))
+                ThrowTerminatingError(_errorRecord);
         }
         protected override void EndProcessing()
         {
@@ -43,6 +62,15 @@ namespace DXDPAPICmdlets
             // Rückkehr, wenn keine Objekte vorhanden sind
             if (_psObjects.Count.Equals(0))
                 return;
+
+            TypeGetter typeGetter = new TypeGetter(this);
+
+            _dataTable = typeGetter.CastObjectsToTableView(_psObjects);
+
+            _dataTable = typeGetter.CastObjectsToTableView(_psObjects); using (PdfDocumentProcessor pdfDocumentProcessor = new PdfDocumentProcessor())
+            {
+
+            }
         }
     }
 }
